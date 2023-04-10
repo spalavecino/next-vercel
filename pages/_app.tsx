4e9118a74b84db440fd24cdedc0d87357c1ff4fc
Sebastian Palavecino <spalavecino@santandertecnologia.com.ar>
2023-04-09 23:49:15 -0300

    Pre-deployment Vercel fix unsaved file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ type AppPropsWithLayout = AppProps & {
   Component: NewxtPageWithLayout
 }
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = (Component.getLayout || ((page) => page))
 
   return getLayout(<Component {...pageProps} />)
